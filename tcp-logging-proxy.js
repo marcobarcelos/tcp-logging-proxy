7fb52ff889dcaa3e7ec48f587a5764dd0e0b8cb8
Marco Barcelos <me@marcobarcelos.com>
2016-11-16 00:39:16 -0200

    Add console colored output

--- tcp-logging-proxy.js
+++ tcp-logging-proxy.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 // -*- js -*-
 
-console.log('starting server');
-var net = require('net');
-var fs = require('fs');
+const net = require('net');
+const fs = require('fs');
+const chalk = require('chalk');
 
 function getParameters() {
 	var args = process.argv.slice(2);
 	if (args < 3) {
-		throw "usage <input port> <output port> <host / ip address>";
+		console.error(chalk.red('Usage: tcp-logging-proxy <input port> <output port> <host / ip address>'));
+		return null;
 	}
 	return args;
 }
@@ -19,12 +20,16 @@ function getFileName() {
 }
 
 function getTimeNow() {
-	return new Date().toUTCString();
+	return new Date()
+		.toISOString()
+		.replace(/T/, ' ')
+		.replace(/\..+/, '');
 }
 
 function getOutputDir() {
 	return './output/';
 }
+
 function buildFilePathBase(fileName) {
 	return getOutputDir() + '/' + fileName;
 }
@@ -37,45 +42,45 @@ function closeLogStreamIfRequired(socketClosed, logStream) {
 	return socketClosed;
 }
 
-var log = function(logStream, message, data) {
-	var logMessage = '\n' + getTimeNow() + ' ' + message;
-	if (data != undefined) {
-		logMessage = logMessage + ': \n' + data;
+function log(logStream, message, data) {
+	var logMessage = chalk.cyan(getTimeNow()) + ' ' + message;
+	if (typeof data !== 'undefined') {
+		logMessage = logMessage + ': ' + data;
 	}
 	
 	logStream.write(logMessage);
 	console.log(logMessage);
 }
 
-var handleConnection = function(incomingSocket) {
+function handleConnection(incomingSocket) {
 
 	var filePathBase = buildFilePathBase(getFileName());
 	var logStream = fs.createWriteStream(filePathBase + '.meta');
 	var upStream = fs.createWriteStream(filePathBase + '.upstream');
 	var downStream = fs.createWriteStream(filePathBase + '.downstream');
 	
-	log(logStream, 'socket connected', incomingSocket.remoteAddress);
+	log(logStream, chalk.green('Client connected'), incomingSocket.remoteAddress);
 
 	var outgoingSocket = net.createConnection(outputPort, host);
 	var socketClosed = false;
 	
-	incomingSocket.on('data', function(data) {
-		log(logStream, 'upstream data', data);
+	incomingSocket.on('data', (data) => {
+		log(logStream, chalk.yellow(`(client ${incomingSocket.remoteAddress}) -> (server)`), data);
 		upStream.write(data);
 	});
 	
-	incomingSocket.on('close', function(data) {
-		log(logStream, 'incoming socket closed');
+	incomingSocket.on('close', (data) => {
+		log(logStream, chalk.red('Incoming socket closed'));
 		socketClosed = closeLogStreamIfRequired(socketClosed, logStream);
 	});
 	
-	outgoingSocket.on('data', function(data) {
-		log(logStream, 'downstream data', data);
+	outgoingSocket.on('data', (data) => {
+		log(logStream, chalk.blue(`(client ${incomingSocket.remoteAddress}) <- (server)`), data);
 		downStream.write(data);
 	});
 	
-	outgoingSocket.on('close', function(data) {
-		log(logStream, 'outgoing socket closed');
+	outgoingSocket.on('close', (data) => {
+		log(logStream, chalk.red('Outgoing socket closed'));
 		socketClosed = closeLogStreamIfRequired(socketClosed, logStream);
 	});
 
@@ -83,18 +88,25 @@ var handleConnection = function(incomingSocket) {
 	outgoingSocket.pipe(incomingSocket);
 };
 
-var handleConnectionDelayed = function(connection) {
-	setTimeout(function() {
+function handleConnectionDelayed(connection) {
+	setTimeout(() => {
 		handleConnection(connection);
 	}, 1000);
 };
 
 function createServer(inputPort, outputPort, host) {
 	var server = net.createServer();
+
+	server.on('listening', () => {
+		console.log(chalk.green('--------------------------------------------------'));
+		console.log(chalk.green(` TCP Proxy of ${host}:${outputPort} running on port ${inputPort} `));
+		console.log(chalk.green('--------------------------------------------------'));
+	});
+
 	server.on('connection', handleConnection);
 
-	server.on('close', function(socket) {
-		console.log('socket closed');
+	server.on('close', (socket) => {
+		console.log('Socket closed');
 	});
 
 	server.listen(inputPort);
@@ -105,17 +117,17 @@ function createDirectory(name, callback) {
 }
 
 parameters = getParameters();
-var inputPort = parameters[0];
-var outputPort = parameters[1];
-var host = parameters[2];
-console.log('input port ' + inputPort + ' output port ' + outputPort + ' ip address / host name ' + host);
 
-createDirectory(getOutputDir(), function(err) {
-	if (err == null || err.code == 'EEXIST') {
-		createServer(inputPort, outputPort, host);
-	} else {
-		console.log('could not create output dir ' + err);
-	}
-}); 
+if (parameters) {
+	var inputPort = parameters[0];
+	var outputPort = parameters[1];
+	var host = parameters[2];
 
-	
+	createDirectory(getOutputDir(), function(err) {
+		if (err === null || err.code === 'EEXIST') {
+			createServer(inputPort, outputPort, host);
+		} else {
+			console.log('Could not create output dir ' + err);
+		}
+	});
+}
